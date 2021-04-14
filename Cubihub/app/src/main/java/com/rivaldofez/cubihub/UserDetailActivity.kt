98c6cd2829765869717c0689ce89388f114c497e
rivaldofez <rivaldofez@gmail.com>
2021-04-15 04:53:56 +0700

    create user search model and dummy tab layout and pager

--- Cubihub/app/src/main/java/com/rivaldofez/cubihub/UserDetailActivity.kt
+++ Cubihub/app/src/main/java/com/rivaldofez/cubihub/UserDetailActivity.kt
@@ -5,12 +5,25 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.rivaldofez.cubihub.adapter.DetailPagerAdapter
 import com.rivaldofez.cubihub.databinding.ActivityUserDetailBinding
 import com.rivaldofez.cubihub.model.User
 import java.lang.Exception
 
 class UserDetailActivity : AppCompatActivity() {
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+    }
+
     lateinit var user:User;
     private lateinit var binding:ActivityUserDetailBinding
 
@@ -20,36 +33,16 @@ class UserDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initView()
-        binding.btnEmail.setOnClickListener {
-            val emailSendIntent = Intent(Intent.ACTION_SEND)
-            emailSendIntent.data = Uri.parse("mailto:")
-            emailSendIntent.type ="text/plain"
-            emailSendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(user.email))
-            emailSendIntent.putExtra(Intent.EXTRA_SUBJECT, "Judul Email")
-            emailSendIntent.putExtra(Intent.EXTRA_TEXT, "Pesan Email")
 
-            try {
-                startActivity(Intent.createChooser(emailSendIntent, "Choose Email Provider"))
-            }catch (e : Exception){
-                Toast.makeText(this, e.message , Toast.LENGTH_SHORT).show()
-            }
-        }
-        binding.btnShare.setOnClickListener {
-            val shareIntent = Intent()
-            shareIntent.action = Intent.ACTION_SEND
-            shareIntent.putExtra(Intent.EXTRA_TEXT, user.fullname)
-            shareIntent.type = "text/plain"
-            try {
-                startActivity(Intent.createChooser(shareIntent, "Share To..."))
-            }catch (e : Exception){
-                Toast.makeText(this, e.message ,Toast.LENGTH_SHORT).show()
-            }
-        }
+        val sectionsPagerAdapter = DetailPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+        supportActionBar?.elevation = 0f
 
-        binding.btnDial.setOnClickListener {
-            val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${user.phone}"))
-            startActivity(dialPhoneIntent)
-        }
     }
 
     private fun initView() {
@@ -57,15 +50,12 @@ class UserDetailActivity : AppCompatActivity() {
 
         binding.tvFullname.text = user.fullname
         binding.tvUsername.text = user.username
-        binding.tvAddress.text = user.address
-        binding.tvAboutMe.text = user.biography
         binding.tvFollower.text = user.num_follower.toString()
         binding.tvFollowing.text = user.num_following.toString()
-        binding.tvRepository.text = "${user.num_repository.toString()} Repo"
+        binding.tvRepository.text = user.num_repository.toString()
 
         val imageResource = resources.getIdentifier(user.avatar,null, packageName)
         Glide.with(applicationContext).load(imageResource).into(binding.imgContent)
 
-        binding.btnDial.setText(user.phone)
     }
 }
